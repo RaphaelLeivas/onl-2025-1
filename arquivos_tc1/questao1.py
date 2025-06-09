2ab3d531fbbb5428dfe39f0d7418e2a68a3879a6
MatheusCarvalho13 <80007569+MatheusCarvalho13@users.noreply.github.com>
2025-06-08 22:11:21 -0300

    Questão1

--- arquivos_tc1/questao1.py
+++ arquivos_tc1/questao1.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 # Função que representa o sistema de controle (sistema de primeira ordem)
 def system(T, t, u):
@@ -26,16 +27,21 @@ def simulate_pid(Kp, Ki, Kd, T_ref, T0, t):
         e_integral += e * (t[1] - t[0])
         # Derivada do erro
         e_derivative = (e - e_previous) / (t[1] - t[0])
+        e_derivative = np.clip(e_derivative, -10000, 10000)  # para tratar a descontinuidade da derivada ao aplicar o degrau
+        #e_integral += np.clip(e_integral, -1000, 1000) # considerando os limites dos atuadores
+        #e = np.clip(e, -1000, 1000) # considerando os limites dos atuadores
 
         # Sinal de controle (PID)
         u = pid_controller(Kp, Ki, Kd, e, e_integral, e_derivative)
 
         # Atualizar a temperatura usando a equação diferencial
-        T = odeint(system, T, [t[i], t[i] + (t[1] - t[0])], args=(u,))[-1]
+        T = odeint(system, T, [t[i], t[i] + (t[1] - t[0])], args=(u,))[-1][0]
 
         # Armazenar a resposta e atualizar o erro anterior
         response.append(T)
         e_previous = e
+       
+    
 
     return np.array(response)
 
@@ -54,15 +60,26 @@ def funcaoobjetivo(x):
 
 
 # Chute inicial para os parâmetros do controlador PID
-ponto_inicial = [1.0, 0.1, 0.01]
+ponto_inicial = [1.0, 1.1, 13]
 
 """ IMPLEMENTE AQUI A CHAMADA DO ALGORTIMO DE OTIMIZAÇÃO """
 
-# Extraia aqui os parâmetros PID otimizados
-Kp_opt, Ki_opt, Kd_opt = ##
+result = minimize(funcaoobjetivo, ponto_inicial, method='BFGS', options={'maxiter': 100000, 'gtol': 0.01})  #  método Quasi Newton
 
+if result.success:
+    Kp_opt, Ki_opt, Kd_opt = result.x
+else:
+    print("Otimização não convergiu:", result.message)
+
+Kp_opt = float(result.x[0])
+Ki_opt = float(result.x[1])
+Kd_opt = float(result.x[2])
+
+print(Kp_opt, Ki_opt, Kd_opt)
+
+t = np.linspace(0, 10, 100)
 # Simulação final com os parâmetros otimizados
-response_opt = simulate_pid(Kp_opt, Ki_opt, Kd_opt)
+response_opt = simulate_pid(Kp_opt, Ki_opt, Kd_opt, 100.0, 25.0, t)
 
 # Plotar a resposta do sistema
 T_ref = 100.0
@@ -73,6 +90,7 @@ plt.xlabel('Tempo')
 plt.ylabel('Temperatura')
 plt.legend()
 plt.title('Resposta do Sistema Controlado (PID Otimizado)')
+plt.ylim(30, 170)  
 plt.show()
 
 # Exibir os parâmetros otimizados
